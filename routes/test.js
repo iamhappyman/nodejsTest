f60ee5f1818514dde84036eaf1249fe1ac1acb80
iamhappyman <phjin9@gmail.com>
2020-12-21 14:31:20 +0900

    시간 수정

--- routes/test.js
+++ routes/test.js
@@ -69,7 +69,9 @@ fetch(url, options)
   .then(json => {
     var datetime = json[0].trade_date_utc + " " + json[0].trade_time_utc
 
-    datetime = Date.parse(datetime);
+    datetime = Date.parse(datetime)
+    //           msec * sec * min * hour 
+    datetime += (1000*60*60*9)
     datetime = new Date(datetime).toLocaleString("ko-KR", {timeZone: "Asia/Seoul"})
     
     res.render('btcprice',
