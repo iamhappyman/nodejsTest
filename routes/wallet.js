439d9927a5f3e56ae1b8e815928a60338d8c4c24
iamhappyman <phjin9@gmail.com>
2020-12-26 17:05:58 +0900

    토큰 등록 기능 구현 완료

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 const { render } = require('pug');
 var router = express.Router();
@@ -22,5 +23,15 @@ router.get("/", async function(req,res,next){
     res.render('wallet',{tokenList:tokenList})
 })
 
+router.post('/regist', function(req,res,next){
+    tokenAddress = req.body.newTokenAddress;
+    if(tokenAddress != ''){
+        tokenList.find( Element => [])
+        tokenList.push({tokenAddress:tokenAddress});
+        fs.writeFileSync('data/tokenList.json',JSON.stringify(tokenList));
+    }
+    res.redirect('/wallet');
+
+})
 
 module.exports = router
